876826a852c7575ee56307c344e7db7a35d1b4b1
Ulugbek <urakov344@gmail.com>
2024-11-20 12:40:31 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,161 @@
+# 1
+# k = int(input("Son kiriting: "))
+# n = int(input("Son kiriting: "))
+# for i in range(n):
+#     print(k)
+
+
+# 2
+# a = int(input("Son kiriting: "))
+# b = int(input("Son kiriting: "))
+#
+# for i in range(a, b+1):
+#     print(i)
+
+
+# 3
+# a = int(input("Son kiriting: "))
+# b = int(input("Son kiriting: "))
+#
+# for i in range(b, a-1, - 1):
+#     print(i)
+
+
+# 4
+# narxi = 10
+# for i in range(1, 11):
+#     print(f'{i} kg konfet narxi ==> {narxi*i}')
+
+# 5
+# narxi = 10
+# for i in range(1, 11):
+#     i/=10
+#     print(f'{i} kg kondet narxi ==> {narxi*i}')
+
+# 6
+# narxi = 10
+# for i in range(12, 21):
+#     i/=10
+#     print(f'{i} kg kondet narxi ==> {narxi*i}')
+
+
+# 7
+# a = int(input('Son kiriting: '))
+# b = int(input('Son kiriting: '))
+# summa = 0
+# for i in range(a, b+1):
+#     summa += i
+# print(summa)
+
+
+# 8
+# a = int(input('Son kiriting: '))
+# b = int(input('Son kiriting: '))
+# summa = 1
+# for i in range(a, b+1):
+#     summa *= i
+# print(summa)
+
+
+# 9
+# a = int(input('Son kiriting: '))
+# b = int(input('Son kiriting: '))
+# summa = 0
+# for i in range(a, b+1):
+#     summa += i**2
+# print(summa)
+
+
+# 10
+# n = int(input('Son kiriting: '))
+# summa = 0
+# for i in range(1, n+1):
+#     summa += 1/i
+# print(summa)
+#
+
+# 11
+# n = int(input('Son kiriting: '))
+# summa = 0
+# for i in range(n, 2 *n+1):
+#     summa += i**2
+# print(summa)
+
+
+# 12
+# n = int(input('Son kiriting: '))
+# summa = 1
+# for i in range(11, n + 11):
+#     i /= 10
+#     summa *= i
+# print(summa)
+
+
+# 13
+n = int(input('Son kiriting: '))
+summa = 0
+for i in range(11, n + 11):
+    i /= 10
+    summa += i
+print(summa)
+
+
+# 14
+# n = int(input('Son kiriting: '))
+# summa = 0
+# for i in range(1, n+1):
+#     summa += 2 * i - 1
+#     print(f'{i} ning kvadrati: {summa}')
+
+
+# 15
+# a = int(input("Sonni kiriting: "))
+# n = int(input("Darajani kiriting: "))
+# res = 1
+# for i in range(n):
+#     res *= a
+# print(res)
+
+# a = int(input("Sonni kiriting: "))
+# n = int(input("Darajani kiriting: "))
+# res = 1
+# i = 0
+# while i < n:
+#     res *= a
+#     i += 1
+# print(res)
+
+
+# 16
+# a = int(input('Son kiriting: '))
+# n = int(input('Son kiriting: '))
+# for i in range(1, n+1):
+#     print(f'{a} ni {i} darajasi ==> {a**i}')
+
+
+# 17
+# a = int(input('Son kiriting: '))
+# n = int(input('Son kiriting: '))
+# summa = 0
+# for i in range(1, n+1):
+#     print(f'{a} ni {i} darajasi ==> {a**i}')
+#     summa += a**i
+# print(f'{a} ni summasi ==> {summa}')
+
+
+# 19
+# n = int(input('Son kiriting: '))
+# summa = 1
+# for i in range(1, n+1):
+#     summa *= i
+# print(summa)
+
+
+# 20
+# n = 3
+# summa = 0
+# faktoriayal = 1
+# for i in range(1, n+1):
+#     faktoriayal *= i
+#     summa += faktoriayal
+# print(summa)
